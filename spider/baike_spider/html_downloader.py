dbbe6f4857c491926b0eadbcc7512bed3004cf3b
guxdde <guxdde@163.com>
2016-02-16 13:43:51 +0800

    Add the code of downloader(网页下载器代码)

--- spider/baike_spider/html_downloader.py
+++ spider/baike_spider/html_downloader.py
@@ -1,10 +1,19 @@
 # -*- coding: utf-8 -*-
 
+import urllib2
+
 
 class HtmlDownloader(object):
     """docstring for HtmlDownloader"""
+
     def __init__(self):
         pass
-        
+
+    # 下载网页内容
     def download(self, new_url):
-        pass
+        if new_url is None:
+            return
+        response = urllib2.urlopen(new_url)
+        if response.getcode() != 200:
+            return None
+        return response.read()
