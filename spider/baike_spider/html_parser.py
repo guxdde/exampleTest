c74dd3ed66ad0c9aff09a679ba8eb69819de9722
guxdde <guxdde@163.com>
2016-02-16 14:25:27 +0800

    Add the code of parser.(添加解析器代码)

--- spider/baike_spider/html_parser.py
+++ spider/baike_spider/html_parser.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+from bs4 import BeautifulSoup
+import re
+import urlparse
+
 
 class HtmlParser(object):
     """docstring for HtmlParser"""
@@ -7,5 +11,36 @@ class HtmlParser(object):
     def __init__(self):
         pass
 
+    def _get_new_urls(self, page_url, soup):
+        new_urls = set()
+        links = soup.find_all('a', href=re.compile(r"/view/\d+\.htm"))
+        for link in links:
+            new_url = link['href']
+            new_full_url = urlparse.urljoin(page_url, new_url)
+            new_urls.add(new_full_url)
+        return new_urls
+
+    def _get_new_data(self, page_url, soup):
+        res_data = {}
+
+        res_data['url'] = page_url
+
+        # <dd class="lemmaWgt-lemmaTitle-title"><h1>Python</h1>
+        title_node = soup.find(
+            "dd", class_="lemmaWgt-lemmaTitle-title").find("h1")
+        res_data['title'] = title_node.get_text()
+
+        # <div class="lemma-summary" label-module="lemmaSummary">
+        summary_node = soup.find("div", class_="lemma-summary")
+        res_data['summary'] = summary_node.get_text()
+
+        return res_data
+
     def parse(self, new_url, html_content):
-        pass
+        if new_url is None or html_content is None:
+            return
+        soup = BeautifulSoup(html_content, "html.parser",
+                             form_encoding="utf-8")
+        new_urls = self._get_new_urls(new_url, soup)
+        new_data = self._get_new_data(new_url, soup)
+        return new_urls, new_data
