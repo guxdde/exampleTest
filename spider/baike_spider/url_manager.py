74420d9a65241db0c29e3a54b30772f0d888e002
guxdde <guxdde@163.com>
2016-02-16 11:53:53 +0800

    Add the code of url_manager(url管理器代码)

--- spider/baike_spider/url_manager.py
+++ spider/baike_spider/url_manager.py
@@ -5,16 +5,29 @@ class UrlManager(object):
     """docstring for UrlManager"""
 
     def __init__(self):
-        pass
+        self.new_urls = set()
+        self.old_urls = set()
 
+    # 向url管理器中添加单个url
     def add_new_url(self, new_url):
-        pass
+        if new_url is None:
+            return
+        if new_url not in self.new_urls and new_url　not in self.old_urls:
+            self.new_urls.add(new_url)
 
+    # 向url管理器中批量添加url
     def add_new_urls(self, new_urls):
-        pass
+        if new_urls is None or len(new_urls) == 0:
+            return
+        for new_url in new_urls:
+            self.add_new_url(new_url)
 
+    # 判断是否有待爬取的url
     def has_new_url(self):
-        pass
+        return len(self.new_urls) != 0
 
+    # 获取待爬取的url
     def get_new_url(self):
-        pass
+        new_url = self.new_urls.pop()
+        self.old_urls.add(new_url)
+        return new_url
