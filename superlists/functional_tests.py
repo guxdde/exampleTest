31907ef53dd5b1874ecd1a5062ff6d5932f994b8
guxdde <guxdde@163.com>
2016-03-01 22:53:11 +0800

    Functional test now checks we can input a to-do item.

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,6 +1,7 @@
 # -*-  coding:utf-8 -*-
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -20,6 +21,24 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
+
+        # 应用邀请他输入一个代办事项
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            input_box.get_attribute('placeholder'), 'Enter a to-do item')
+
+        # 他在一个文本矿中输入了“Buy peacock feathers”(购买孔雀羽毛)
+        # 伊迪斯等爱好时使用假蝇做鱼饵钓鱼
+        input_box.send_keys('Buy peacock feathers')
+
+        # 他按回车键后，页面更新了
+        # 代办事项表格中显示了“1：Buy peacock feathers”
+        input_box.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1:Buy peacock feathers' for row in rows))
 
         self.fail('Finish the test!')
 
