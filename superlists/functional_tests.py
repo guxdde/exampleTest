15f7dafef5c658f5dd9452bead66aeb3e87b4fca
guxdde <guxdde@163.com>
2016-03-03 22:25:35 +0800

    重构功能测试

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -15,6 +15,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get("http://localhost:8000")
 
@@ -36,22 +41,27 @@ class NewVisitorTest(unittest.TestCase):
         # 代办事项表格中显示了“1：Buy peacock feathers”
         input_box.send_keys(Keys.ENTER)
 
-        # time.sleep(10)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        self.check_for_row_in_list_table('1:Buy peacock feathers')
+        time.sleep(10)
+        # table = self.browser.find_element_by_id('id_list_table')
+        # rows = table.find_elements_by_tag_name('tr')
         # self.assertTrue(
-        #     any(row.text == '1:Buy peacock feathers' for row in rows),
-        #     "New to-do item did not appear in table--its text was:\n%s" % (table.text,))
-        self.assertIn('1:Buy peacock feathers', [row.text for row in rows])
+        # any(row.text == '1:Buy peacock feathers' for row in rows),
+        # "New to-do item did not appear in table--its text was:\n%s" % (table.text,))
+        # self.assertIn('1:Buy peacock feathers', [row.text for row in rows])
 
-        #他输入了“Use peacock feathers to make a fly”（使用孔雀毛做假蝇）
+        # 他输入了“Use peacock feathers to make a fly”（使用孔雀毛做假蝇）
         input_box = self.browser.find_element_by_id('id_new_item')
         input_box.send_keys('Use peacock feathers to make a fly')
         input_box.send_keys(Keys.ENTER)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1:Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2:Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1:Buy peacock feathers')
+        self.check_for_row_in_list_table(
+            '2:Use peacock feathers to make a fly')
+        # table = self.browser.find_element_by_id('id_list_table')
+        # rows = table.find_elements_by_tag_name('tr')
+        # self.assertIn('1:Buy peacock feathers', [row.text for row in rows])
+        # self.assertIn(
+        #     '2:Use peacock feathers to make a fly', [row.text for row in rows])
 
         self.fail('Finish the test!')
 
